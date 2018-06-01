ecd1881f8364acbbe6dfebada8176257bdec875f
Flavio Takeuchi <flaviotakeuchi@Mac-mini-de-Flavio.local>
2018-06-01 17:47:24 -0300

    Testes para gerar boleto boleto

--- cielo.test.js
+++ cielo.test.js
@@ -86,3 +86,40 @@ test('cardBin', async (t) => {
 
   t.end()
 })
+
+test('Boleto', async (t) => {
+  const boletoParams = {
+    "MerchantOrderId": "20180531",
+    "Customer": {
+      "Name": "Comprador Teste Boleto",
+      "Identity": "1234567890",
+      "Address": {
+        "Street": "Avenida Marechal Câmara",
+        "Number": "160",
+        "Complement": "Sala 934",
+        "ZipCode": "22750012",
+        "District": "Centro",
+        "City": "Rio de Janeiro",
+        "State": "RJ",
+        "Country": "BRA"
+      }
+    },
+    "Payment": {
+      "Type": "Boleto",
+      "Amount": 15700,
+      "Provider": "Bradesco2",
+      "Address": "Rua Teste",
+      "BoletoNumber": "123",
+      "Assignor": "Empresa Teste",
+      "Demonstrative": "Desmonstrative Teste",
+      "ExpirationDate": "5/1/2015",
+      "Identification": "11884926754",
+      "Instructions": "Aceitar somente até a data de vencimento, após essa data juros de 1% dia."
+    }
+  }
+  const boleto = await cielo.boleto.sale(boletoParams)
+
+  t.assert(typeof boleto.Code === 'undefined', 'Não ocorreu erro no boleto')
+  t.assert(typeof boleto.Payment !== 'undefined', 'Boleto retornou Payment')
+  t.assert(boleto.Payment.Url.trim() !== '', 'Retornou Url para o boleto')
+})
